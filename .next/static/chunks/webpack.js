619c6d7f7f7eb0c9f3710600fe1c120f9984366d
rngnnn <rngnnnclk@gmail.com>
2025-04-21 11:18:47 +0200

    Update webpack cache and hot-update files
    - Added new cache files for webpack server development.
    - Created hot-update JSON files for webpack with updated content.
    - Introduced new hot-update JavaScript files with eval-source-map for development.
    - Updated hash values in hot-update files to reflect recent changes.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44701cfc8e164b85"; }
+/******/ 		__webpack_require__.h = function() { return "3403d5ba125210fd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
