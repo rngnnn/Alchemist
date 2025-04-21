010250529e8ca6674638f6389142eccd610cc4de
rngnnn <rngnnnclk@gmail.com>
2025-04-21 14:36:11 +0200

    contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,11 +1,6 @@
-import Head from 'next/head';
 import { useState } from 'react';
-import swr from '../lib/swr';
 
 export default function Contact() {
-  const { data: _projects } = swr('/api/projects');
-  const projects = _projects ? _projects : null;
-
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -25,106 +20,102 @@ export default function Contact() {
   };
 
   return (
-    <>
-      <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Contact Me</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">
-          Other addresses where you can contact me.
-        </p>
-        <div className="flex justify-center flex-col items-center">
-          <div className="max-w-lg w-full">
-            {/* Sosyal Medya Bağlantıları */}
-            <a target="_blank" rel="norefferer" href="s">
-              <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                <i className="fab fa-discord fa-2x" />
-                <p className="font-semibold text-xl">@rngn</p>
+    <section className="py-24 bg-red-900">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-10">
+          {/* Görsel ve İletişim Bilgileri */}
+          <div className="relative">
+            <img
+              src="assets/techs/rngn.png"
+              alt="Contact Us Illustration"
+              className="w-full lg:h-auto h-[500px] lg:rounded-l-xl rounded-2xl object-contain"
+            />
+           
+            <div className="absolute bottom-0 w-full p-6 bg-red-900 rounded-lg">
+              <div className="mb-4">
+                <h5 className="text-black text-lg font-semibold">Phone</h5>
+                <p className="text-gray-50">+31685166208</p>
               </div>
-            </a>
-            <a target="_blank" rel="norefferer" href="">
-              <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                <i className="fab fa-instagram fa-2x" />
-                <p className="font-semibold text-xl">@rngn</p>
+              <div className="mb-4">
+                <h5 className="text-black text-lg font-semibold">Email</h5>
+                <p className="text-gray-50">rngnnnclk@gmail.com</p>
               </div>
-            </a>
-            <a target="_blank" rel="norefferer" href="">
-              <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                <i className="fab fa-telegram fa-2x" />
-                <p className="font-semibold text-xl">@rngn</p>
+              <div>
+                <h5 className="text-black text-lg font-semibold">Address</h5>
+                <p className="text-gray-50">
+                  Amsterdam
+                </p>
               </div>
-            </a>
-            <a href="">
-              <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                <i className="fa fa-envelope fa-2x" />
-                <p className="font-semibold text-xl">rngnnnclk@gmail.com</p>
-              </div>
-            </a>
+            </div>
           </div>
 
-          {/* İletişim Formu */}
-          <form
-            onSubmit={handleSubmit}
-            className="bg-white shadow-md rounded-lg p-6 w-full max-w-lg mt-10"
-          >
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-gray-700 font-bold mb-2"
+          {/* Form Alanı */}
+          <div className="bg-red p-8 rounded-2xl shadow-lg">
+            <h2 className="text-black text-3xl font-bold mb-6">
+              Send Me A Message
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-indigo-300"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-indigo-300"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-gray-700 font-bold mb-2"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-indigo-300"
+                  rows="4"
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-black-600 text-white font-bold py-2 px-4 rounded-lg hover:bg-red-700 transition duration-200"
               >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="message"
-                className="block text-gray-700 font-bold mb-2"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-                rows="4"
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
-            >
-              Send Message
-            </button>
-          </form>
+                Send
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </>
+    </section>
   );
 }
